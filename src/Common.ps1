94ab483c1c6eae8e6906544c0e101aebbfd8ae95
RJ Cuthbertson <cuthbertson.richard@gmail.com>
2017-12-10 13:09:35 -0500

    added file header

--- src/Common.ps1
+++ src/Common.ps1
@@ -1,3 +1,30 @@
+<#
+  Common UX Functions:
+    This file is for general purpose, utility functions intended to be
+    used throughout the other scripts in this repository. Functions in
+    this file handle display formatting and / or presentation, and are
+    placed here to allow for a consistent UX across the other scripts
+    in this repository.
+
+  Copyright (C) 2017  RJ Cuthbertson
+
+  This script can be found on GitHub:
+    https://github.com/RJCuthbertson/PowerShellScripts
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#>
+
 Function Clear-Workspace()
 {
   $cursorTop = [System.Console]::CursorTop
